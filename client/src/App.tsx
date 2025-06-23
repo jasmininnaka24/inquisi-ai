2bd622fc0bd44c3533460657454d9ae4098e403e
jasmininnaka24 <jasmininnaka@gmail.com>
2025-06-23 19:59:32 +0800

    updated some code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,13 +32,13 @@ export default function QuizApp() {
   const fetchQuestion = async (categoryName: string) => {
     setIsLoading(true)
     try {
-      const response = await fetch("http://127.0.0.1:8000/category", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ category: categoryName }),
-      })
+      });
 
       if (!response.ok) {
         throw new Error("Failed to fetch question")
